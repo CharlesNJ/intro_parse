6d551cc4e8355452cd83fd02fc58f6bef5397052
CharlesNJohn <CharlesNJohn@users.noreply.github.com>
2019-08-06 22:26:50 -0500

    Listview with title, subtitle and leading with circle avatar

--- lib/main.dart
+++ lib/main.dart
@@ -6,30 +6,62 @@ import 'package:http/http.dart' as http;
 
 
 void main() async{
-  String _data = await getJson();
+  List _data = await getJson();
+  String _body = '';
 
-  print (_data);
-  runApp(new MaterialApp(
-    home: new Scaffold(
-      appBar: new AppBar(
-        backgroundColor: Colors.amber,
-        title: new Text('JSON Parse'),
-        centerTitle: true,
 
+  for(int i=0; i< _data.length; i++) {
+    print(_data[i]['title']);
+    print('body: ${_data[i]['body']}');
+  }
+  _body= _data[0]['body'];
+    runApp(new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          backgroundColor: Colors.amber,
+          title: new Text('JSON Parse'),
+          centerTitle: true,
+
+        ),
+        body: new Center(
+          child: new ListView.builder(
+            itemCount: _data.length,
+              padding: const EdgeInsets.all(14.5),
+
+              itemBuilder: (BuildContext context, int position){
+              return Column(
+                children: <Widget>[
+                  new Divider(height: 5.5),
+                  new ListTile(
+                    title: Text("${_data[position]['title']}"),
+
+                    subtitle: Text("${_data[position]['body']}",
+                      style: new TextStyle(fontSize: 13.9),),
+
+                    leading: new CircleAvatar(
+                      backgroundColor: Colors.yellowAccent,
+                      child: Text("${_data[position]['body'][0]}",
+                        style: new TextStyle(fontSize: 20.9, color: Colors.orange)),
+                      ),
+
+
+
+                  )
+                ],
+              );
+              })
+        ),
       ),
-      body: new Center(
-        child: new Text('Parsing..'),
-      ),
-    ),
-  ));
-}
+    ));
+  }
 
-Future<String> getJson() async{
+
+Future<List> getJson() async{
   String apiUrl= 'https://jsonplaceholder.typicode.com/posts';
 
   http.Response response = await http.get(apiUrl);
 
-  return json.decode(response.body).toString();
+  return json.decode(response.body);
 
 
 }
